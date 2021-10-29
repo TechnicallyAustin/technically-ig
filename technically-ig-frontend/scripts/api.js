3e9de065406e18e395d57dd13bc31b6fa8cb218e
Austin <Technicallyaustin@gmail.com>
2021-10-28 23:15:41 -0700

    formattin code

--- technically-ig-frontend/scripts/api.js
+++ technically-ig-frontend/scripts/api.js
@@ -23,14 +23,11 @@ function fetchPosts() {
         })
 }
 
-
-
 function createPosts() {
     newPostUl = document.body.appendChild(document.createElement('ul-posts'))
 
     allPosts.forEach(post => {
         postBuilder()
-
         // Profile Div 
         profileName.innerText = "Profile Name"
 
@@ -44,14 +41,10 @@ function createPosts() {
 
         // Likes Div
         newLikesDiv.innerText = `${post['likes']} likes`
-
-
     })
-
 }
 
 function postBuilder() {
-
     // Post List items
     newPostLi = newPostUl.appendChild(document.createElement('li-posts'))
 
@@ -60,77 +53,17 @@ function postBuilder() {
     profileName = newProfileDiv.appendChild(document.createElement('p'));
 
     // Image Div
-    newImageDiv = newProfileDiv.appendChild(document.createElement('div-image'))
-    addImage = newImageDiv.appendChild(document.createElement('img'))
+    newImageDiv = newProfileDiv.appendChild(document.createElement('div-image'));
+    addImage = newImageDiv.appendChild(document.createElement('img'));
     
     // Actions Div
-    newActionDiv = newImageDiv.appendChild(document.createElement('div-action'))
-    let actionController = newActionDiv.appendChild(document.createElement('p'))
-    actionController.innerText = " Like | Comment | Send | Saved"
+    newActionDiv = newImageDiv.appendChild(document.createElement('div-action'));
+    let actionController = newActionDiv.appendChild(document.createElement('p'));
+    actionController.innerText = " Like | Comment | Send | Saved";
   
     // Comments Div
-    newCommentDiv = newActionDiv.appendChild(document.createElement('div-comments'))
-    
+    newCommentDiv = newActionDiv.appendChild(document.createElement('div-comments'));
 
     // Likes Div
-    newLikesDiv = actionController.appendChild(document.createElement('p'))
-    
-
+    newLikesDiv = actionController.appendChild(document.createElement('p'));
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-function createPosts() {
- // Provides the url for each post as li in an Ul 
- let newUl = document.body.appendChild(document.createElement('ul'));
- let ul = document.querySelector('ul');
- let li = document.querySelector('li');
-
-
- allPosts.forEach(post => {
-     newPostLi = ul.appendChild(document.createElement('li'));
-     
-
-     // Create a div for the user profile image source and account name 
-       let newDiv = newLi.appendChild(document.createElement('div-card'))
-       let title = newDiv.appendChild(document.createElement('p'))
-       title.innerText = "Profile Name"
-         
-
-     // Img importing and Formatting 
-     let newImg = newDiv.appendChild(document.createElement('img'));
-     newImg.src = post['url'];
-     newImg.alt = post['caption'];
-     newImg.width = "600";
-     newImg.height = "500";
-
-     // Create a Div to hold likes, comments, sending, and Saving 
-     let newPostDiv = newDiv.appendChild(document.createElement('div-post'))
-
-     let newPostActions = newDiv.appendChild(document.createElement('div-actions'))
-
-     newPostActions.innerText = "Like | Comment | Send"
-
-     let newCommentDiv = newPostDiv.appendChild(document.createElement('div-comments'))
-
-     newCommentDiv.innerText = "Comments Section"
- })
-}
-*/
-
-
-/* Function to take post URL's and embed them as images, Also Formats images.*/
-
-/* Function */
